a4021e11773ed4bb78f498fa85164cb732e0243f
Jason Mullen <75854582+JasonMullen@users.noreply.github.com>
2023-06-19 09:01:37 -0500

    Add files via upload

---
+++ chapter13Practice_1/chapter13Practice_1.cpp
@@ -0,0 +1,17 @@
+/*
+   Jason mullen 
+   9-19-20
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int x;
+	x << 5;
+	cout << x << endl;
+	cout << 5 << endl;
+	return 0;
+}
+
